e59e66b2da86ffb2ebeb38eeca5c85317b40bbfa
Erick Marcia <mar_cia14@hotmail.com>
2021-05-26 11:08:49 -0600

    Contiene modificaciones en la Bio y post  Xamarin-MAUI

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -21,9 +21,10 @@
 <div>
   <img src="/media/about-me/profile.png" alt="Erick Marcia">
   <p>
-    Hola, Soy <strong>Erick Marcia</strong>. Soy a software engineer and data scientist from Córdoba, Nicaragua.
+    Hola, Soy <strong>Erick Marcia</strong>. Ingeniero de Software y MCSA certificado por Microsoft, vivo en Granada -
+    Nicaragua.
     Puedes seguirme en <a href="https://www.twitter.com/Emarcia14" target="_blank">Twitter</a>,
     ver algunos de mis trabajos en <a href="https://www.github.com/erickmarcia" target="_blank">GitHub</a>,
-    o leer más sobre mí en <a href="https://www.erickmarcia.com" target="_blank">my website</a>.
+    o leer más sobre mí en <a href="https://erickmarcia.github.io/" target="_blank">my website</a>.
   </p>
 </div>
