57a2c67a3ba4ded1d463ea542ceb098d56d91681
Erick Marcia <mar_cia14@hotmail.com>
2022-01-09 03:28:52 -0600

    Updates

---
+++ client/skill.cacd26ee.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as a,n as e}from"./client.1df7a3ae.js";function n(s){return{c:e,l:e,m:e,p:e,i:e,o:e,d:e}}export default class extends s{constructor(s){super(),t(this,s,null,n,a,[])}}
